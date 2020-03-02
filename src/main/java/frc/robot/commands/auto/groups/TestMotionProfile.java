42b59340b7e1b5902b130f2730da51e95087b7b7
wozeparrot <wozeparrot@gmail.com>
2020-03-02 10:39:29 -0500

    More cleanup

--- src/main/java/frc/robot/commands/auto/groups/TestMotionProfile.java
+++ src/main/java/frc/robot/commands/auto/groups/TestMotionProfile.java
@@ -1,6 +1,5 @@
 package frc.robot.commands.auto.groups;
 
-import bbb.path.Gains;
 import bbb.path.PathFollowerSettings;
 import bbb.path.UltraPathFollower;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
